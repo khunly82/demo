7a708c700735f8227a0fe7a858e45f0f22e677b6
Khun <lykhun@gmail.com>
2024-01-26 14:03:36 +0100

    affichage html

--- src/assets/script.js
+++ src/assets/script.js
@@ -16,7 +16,25 @@ const init = () => {
             row.push(0);
         }
     }
-    console.log(grid);
+    displayGrid();
+}
+
+const displayGrid = () => {
+    gridElement.append(...grid.map(createHtmlRow));
+}
+
+const createHtmlRow = (row) => {
+    const div = document.createElement('div');
+    div.classList.add('row');
+    div.append(...row.map(createHtmlCell));
+    return div;
+}
+
+const createHtmlCell = (cell) => {
+    const div = document.createElement('div');
+    div.classList.add('cell');
+    div.innerText = cell;
+    return div;
 }
 
 init();
