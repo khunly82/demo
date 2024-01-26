65fd097cee7f8bb634d1f857f861b3f55fa20120
Khun <lykhun@gmail.com>
2024-01-26 16:37:53 +0100

    demineur

--- src/assets/script.js
+++ src/assets/script.js
@@ -31,6 +31,19 @@ const addBombs = () => {
         const y = Math.floor(Math.random() * height);
         if(grid[y][x] !== '*') {
             grid[y][x] = '*';
+            
+            for(let dy = -1; dy <= 1; dy++) {
+                for(let dx = -1; dx <= 1; dx++) {
+                    if(y+dy<0 || y+dy>=height || x+dx<0 || x+dx>=width) {
+                        continue;
+                    }
+                    if(grid[y+dy][x+dx] === '*') {
+                        continue;
+                    }
+                    grid[y+dy][x+dx]++;
+                }
+            }
+
             addedBombs++;
         } 
     }
@@ -41,19 +54,43 @@ const displayGrid = () => {
     gridElement.append(...grid.map(createHtmlRow));
 }
 
-const createHtmlRow = (row) => {
+const createHtmlRow = (row, y) => {
     const div = document.createElement('div');
     div.classList.add('row');
-    div.append(...row.map(createHtmlCell));
+    div.append(...row.map(createHtmlCell(y)));
     return div;
 }
 
-const createHtmlCell = (cell) => {
+const createHtmlCell = y => (cell, x) => {
     const div = document.createElement('div');
     div.classList.add('cell');
-    div.innerText = cell;
+    const displayCell = (x,y) => {
+        console.log(x, y)
+        const selected = document.querySelector(
+            `.row:nth-child(${y+1})>.cell:nth-child(${x+1})`
+        );
+        if(selected.innerText !== '') {
+            return;
+        }
+        selected.innerText = grid[y][x];
+        if(grid[y][x] === 0) {
+            for(let dy = -1; dy <= 1; dy++) {
+                for(let dx = -1; dx <= 1; dx++) {
+                    if(y+dy<0 || y+dy>=height || x+dx<0 || x+dx>=width) {
+                        continue;
+                    }
+                    if(grid[y+dy][x+dx] === '*') {
+                        continue;
+                    }
+                    displayCell(x+dx, y+dy);
+                }
+            }
+        }
+    }
+    div.addEventListener('click',() => displayCell(x, y))
     return div;
 }
+
 
 const changeOptions = () => {
     const h = prompt('Entrez la hauteur');
@@ -69,6 +106,7 @@ const changeOptions = () => {
 changeButton.addEventListener('click', changeOptions);
 
 init();
+
 
 
 
